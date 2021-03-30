8d78e29cb9a75391d2ef68a81f994cd32f8176d1
afcomesana <48021565+afcomesana@users.noreply.github.com>
2021-03-30 15:11:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(port, error => {
     if (error) throw error;
